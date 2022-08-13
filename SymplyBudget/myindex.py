62d196edf2c02487b109f82600f41e6a758cb9d9
Jonas-OGabriel <joao.gabriel.araujo.salvador@gmail.com>
2022-08-13 20:52:02 -0300

    Primeiros passos no uso do Framework

--- SymplyBudget/myindex.py
+++ SymplyBudget/myindex.py
@@ -6,6 +6,9 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
+from components import sidebar
+from components import extratos
+from components import dashboards
 
 
 
@@ -15,14 +18,25 @@ content = html.Div(id="page-content")
 
 
 app.layout = dbc.Container(children=[
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id='url'), 
+            sidebar.layout
+        ], md=2, style={'background-color': 'red', 'height': '100vh'}),
 
-
-
-
-
+        dbc.Col([
+            content
+        ], md=10, style={'background-color': 'blue', 'height': '100vh'})
+    ])
 ], fluid=True,)
 
 
+@app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
+def render_page(pathname):
+    if pathname == '/' or pathname == '/dashboards':
+        return dashboards.layout
+    elif pathname == '/extratos':
+        return extratos.layout
 
 
 if __name__ == '__main__':
